ab6ec4762e17263f91a86ccd932c2471fd0dd8e7
FDI <isanchod@gmail.com>
2025-11-03 16:04:12 +0100

    Añado el valor id a la clase CharacterApiModel.kt

--- app/src/main/java/edu/iesam/lossimpson/features/characters/data/remote/CharacterApiModel.kt
+++ app/src/main/java/edu/iesam/lossimpson/features/characters/data/remote/CharacterApiModel.kt
@@ -1,6 +1,7 @@
 package edu.iesam.lossimpson.features.characters.data.remote
 
 data class CharacterApiModel(
+    val id: String,
     val nombre: String,
     val status: String,
     val ocupation: String,
