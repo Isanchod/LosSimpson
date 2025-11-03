ef53039dd8286b2d0518cfadf355141912c82463
FDI <isanchod@gmail.com>
2025-11-03 15:41:34 +0100

    Añado la estructura al CharacterApiModel.kt

--- app/src/main/java/edu/iesam/lossimpson/features/characters/data/remote/CharacterApiModel.kt
+++ app/src/main/java/edu/iesam/lossimpson/features/characters/data/remote/CharacterApiModel.kt
@@ -1,2 +1,9 @@
 package edu.iesam.lossimpson.features.characters.data.remote
 
+data class CharacterApiModel(
+    val nombre: String,
+    val status: String,
+    val ocupation: String,
+    val portrait_path : String
+)
+
