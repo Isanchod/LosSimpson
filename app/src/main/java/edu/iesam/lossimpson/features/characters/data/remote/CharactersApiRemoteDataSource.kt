41f0323166cf402178e698ae4bd67c662491398e
Isanchod <isanchod@gmail.com>
2025-11-06 17:15:04 +0100

    Implemento un try catch para que cuando no haya red devolver en el result fallido el NetworkError

--- app/src/main/java/edu/iesam/lossimpson/features/characters/data/remote/CharactersApiRemoteDataSource.kt
+++ app/src/main/java/edu/iesam/lossimpson/features/characters/data/remote/CharactersApiRemoteDataSource.kt
@@ -5,17 +5,23 @@ import edu.iesam.lossimpson.features.characters.domain.ErrorApp
 import edu.iesam.lossimpson.features.characters.domain.SimpsonCharacter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.net.UnknownHostException
 
 class CharactersApiRemoteDataSource (private val apiClient: ApiClient) {
     suspend fun get20FirstCharacters(): Result<List<SimpsonCharacter>> {
         return withContext(Dispatchers.IO) {
-            val apiService = apiClient.createService(CharacterApiService::class.java)
-            val apiResponse = apiService.findFirst20Characters()
-            if(apiResponse.isSuccessful && apiResponse.errorBody() == null){
-                Result.success( apiResponse.body()!!.toCharacter()
-                )
-            } else {
-                Result.failure(ErrorApp.ApiError)
+            try {
+                val apiService = apiClient.createService(CharacterApiService::class.java)
+                val apiResponse = apiService.findFirst20Characters()
+                if (apiResponse.isSuccessful && apiResponse.errorBody() == null) {
+                    Result.success(
+                        apiResponse.body()!!.toCharacter()
+                    )
+                } else {
+                    Result.failure(ErrorApp.ApiError)
+                }
+            } catch (e: UnknownHostException) {
+                Result.failure(ErrorApp.NetworkError)
             }
         }
     }
